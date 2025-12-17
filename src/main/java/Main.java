d7893c52c5140ad467678c2bc162a21a1ec8da26
Semir MALICKY <arkaentech@gmail.com>
2025-12-17 13:41:13 +0100

    first commit for the hello world countdown

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public static void main(String[] args) {
+    int i=10;
+    do {
+        System.out.println(i+"...");
+    }while ((i--)>1);
+    System.out.println("Hello World!!");
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.vdekennet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
