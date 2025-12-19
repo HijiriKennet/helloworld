10d07673c804b553216f7b566b55038d19354fab
HijiriKennet <kennethmal@hotmail.fr>
2025-12-19 11:32:40 +0100

    Wrap main method in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
-
-public static void main(String[] args) {
-    int i=10;
-    do {
-        System.out.println(i+"...");
-    }while ((i--)>1);
-    System.out.println("Hello World!!");
+public class Main {
+    public static void main(String[] args) {
+        int i=10;
+        do {
+            System.out.println(i+"...");
+        }while ((i--)>1);
+        System.out.println("Hello World!!");
+    }
 }
